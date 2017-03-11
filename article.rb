1dbea872a0a5e22afe64664717582f8d0d741719
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-03-11 23:58:07 +0100

    stub better version

--- article.rb
+++ article.rb
@@ -6,69 +6,99 @@ require 'open3'
 require 'fileutils'
 require 'pdf-reader'
 
+require 'open3'
+
 require "i18n"
 I18n.config.available_locales = :en
 
+require './prompt'
+
+def apply_range_list(ranges:, iterable:)
+  selection = []
+  ranges.each do |range|
+    selection << iterable[range]
+  end
+  selection
+end
+
 def select_lines(
-  query: 'Input a list of line ranges: ',
-  default: '0..1',
+  question: 'Input a list of line ranges: ',
+  lead: '0..1',
   lines: []
 )
   lines.each_with_index do |line, index|
     puts "#{index}:  ".green + line.rstrip.blue
   end
 
-  ranges_string = ask(query) do |input|
-    input.default = default
-    input.validate = /(\d+\.\.\d+|\d+)(\s*,\s*\d+\.\.\d+|\d+)*/
-  end
+  puts lead
 
-  ranges = ranges_string.split(/\s*,\s*/).map do |range|
-    if range =~ /\d+\.\.\d+/
-      Range.new(*range.split('..').map(&:to_i))
-    elsif range =~ /\d+/
-      range.to_i
-    else
-      raise 'There definitely is a bug, bub.'
-    end
-  end
+  ranges = RangesPrompt.new(
+    question: question.blue,
+    lead: lead
+  ).ask
 
-  selected_lines = []
-  ranges.each do |range|
-    selected_lines << lines[range]
-  end
-
-  selected_lines
+  apply_range_list(ranges: ranges, iterable: lines)
 end
 
 begin
   # Get filename and check existence.
   filename = ARGV[0]
+  raise 'I need a filename, bub.' if filename.nil?
   raise 'File does not exist, bub.' unless File.exist? filename
 
   # Clean filename and show it.
   filename = File.absolute_path ARGV[0]
-  puts "Filename:\n  #{filename}"
+  puts "File found:\n  #{filename}"
+
+  # Select the page range for extraction.
+  page_range = QualifiedRangePrompt.new(
+    question: "Input a page range for extraction: ".blue,
+    lead: '0[0..9]'
+  ).ask
+
+  # puts page_range.inspect.yellow
+
 
   # Read file and get text from first page removing empty lines.
   reader = PDF::Reader.new filename
-  front_page_text = reader.page(1).text.gsub(/\n+/, "\n")
+  txt = reader.pages[2..4]
+
+  # puts txt.inspect
+
+  lines = []
+  page_range.each do |spec|
+    reader.pages[spec[:pages]].each do |page|
+      lines.concat page.text.gsub(/\n+/,"\n").lines[spec[:lines]]
+    end
+  end
+
+  lines.each_with_index do |line, index|
+    line_number = index.to_s.rjust((lines.length-1).to_s.length)
+    puts "#{line_number}: ".green + line.rstrip.blue
+  end
+
+  # exit
+  # stdout, stderr, status = Open3.capture3(
+  #   'pdftotext', '-f', page_range.first.to_s, '-l', page_range.last.to_s, filename, '-'
+  # )
+  # raise "There was an error parsing the PDF, bub." unless status.success?
+  # extracted_text = stdout.gsub(/\n+/, "\n")
 
   # Check whether there actually is text.
-  raise "There is no text to fetch, bub." if front_page_text.empty?
+  raise "There is no text to fetch, bub." if extracted_text.empty?
 
   # Search and fetch some kind of header on first page.
-  # if (/abstract/i).match(front_page_text)
+  # if (/abstract/i).match(extracted_text)
   #   # Get everything before abstract.
-  #   header = front_page_text.split(/abstract/i).first
-  # elsif (/@/).match(front_page_text)
+  #   header = extracted_text.split(/abstract/i).first
+  # elsif (/@/).match(extracted_text)
   #   # Cut everything after last email.
-  #   header = front_page_text.rpartition(/@\S+/).first(2).join
+  #   header = extracted_text.rpartition(/@\S+/).first(2).join
   # else
 
   puts "Head of first page:"
-  header = front_page_text.lines
-  header = select_lines(lines: header[0,9]).join
+  header = extracted_text.lines
+  header = select_lines(lines: header).join
   # header = header.join
 
   # end
@@ -76,9 +106,13 @@ begin
   # Prepare search string from matched header.
   search_string = header.gsub(/\s+/, ' ').strip
   search_string = I18n.transliterate search_string
-  puts "Search string:\n  " + search_string.scan(/.{1,78}/).join("\n  ")
+  search_string = Prompt.new(
+    question: 'Confirm search string: '.blue,
+    lead: search_string,
+    regexp: //
+  ).ask
 
-  # exit
+  exit
 
   # Use scholar.py to fetch the best (first) match.
   result, stderr, status = Open3.capture3(
@@ -97,7 +131,7 @@ begin
 
   # Extract and format authors.
   authors = result.scan(/%A (.*),/).map(&:first).map { |a| I18n.transliterate a }
-  safe_authors = authors.uniq.join('_').upcase
+  safe_authors = authors.uniq.join('_').upcase.gsub(/\s+/, '_')
 
   # Format new filename.
   puts safe_authors

4b5921a41078ebd9e6cdee9f0e3bc108f4d8880f
Paolo Brasolin <paolo@leanbit.it>
2017-07-05 22:08:06 +0200

    finish up scholarize with prompts

--- scholarize.rb
+++ scholarize.rb
@@ -1,10 +1,13 @@
 #!/usr/bin/env ruby
+# coding: utf-8
 
 require 'colorize'
 require 'open3'
 
 require "i18n"
 I18n.config.available_locales = :en
+
+require_relative 'lib/prompt'
 
 require_relative 'lib/options'
 BabDub::Options.parse
@@ -34,35 +37,74 @@ ok = "\u2714"
 ko = "\u2718"
 qm = "\u272E"
 
+puts "\nRemember, bub: when prompted ENTER to confirm, CTRL-U + ENTER to skip.".green
+
 BabDub::Options.get[:input_files].each do |source_path|
   puts "\n"
+
   puts qm.yellow + ' ' + source_path
+
   query_string = sanitize_file_path source_path
-  query_result = query_scholar query_string
+  _, chosen_query = BabDub::Prompts::Prompt.new(
+       question: "Proposed query:\n  ".cyan,
+       lead: query_string,
+       regexp: //
+     ).ask
 
-  # This exception should never raise in normal use cases:
-  raise query_result[:stderr] unless query_result[:exit_code].success?
-
-  if !query_result[:stdout].empty? # not an infallible check, but hey.
-    safe_title = I18n.transliterate(query_result[:title])
-                   .downcase.gsub(/[^a-z0-9 ]/i, ' ')
-                   .strip.gsub(/\s+/, '_')
-    safe_authors = query_result[:authors]
-                     .map { |a| I18n.transliterate a }
-                     .uniq.join('_')
-                     .upcase.gsub(/\s+/, '_')
-    target_dirname = BabDub::Options.get[:output_folder] || File.dirname(source_path)
-    target_basename = [safe_title, '_', safe_authors, File.extname(source_path)].join
-    target_path = File.join target_dirname, target_basename
-    puts ok.green + ' ' + target_path
+  if chosen_query.empty?
+    puts ko.red + ' File skipped, bub.'
   else
-    target_dirname = BabDub::Options.get[:reject_folder] || File.dirname(source_path)
-    target_basename = File.basename(source_path)
-    target_path = File.join target_dirname, target_basename
-    puts ko.red + ' ' + target_path
-  end
+    query_result = query_scholar chosen_query
 
-  File.rename source_path, target_path unless BabDub::Options.get[:dry_run]
+    # This exception should never raise in normal use cases:
+    raise query_result[:stderr] unless query_result[:exit_code].success?
+
+    if !query_result[:stdout].empty? # not an infallible check, but hey ¯\_(ツ)_/¯
+      puts "Query result:".cyan
+      puts query_result[:stdout].strip.gsub(/^%./) { |m| "  " + m.cyan }
+      safe_title = I18n.transliterate(query_result[:title])
+                    .downcase.gsub(/[^a-z0-9 ]/i, ' ')
+                    .strip.gsub(/\s+/, '_')
+      safe_authors = query_result[:authors]
+                      .map { |a| I18n.transliterate a }
+                      .uniq.join('_')
+                      .upcase.gsub(/\s+/, '_')
+      target_dirname = BabDub::Options.get[:output_folder] || File.dirname(source_path)
+      target_basename = [safe_title, '_', safe_authors, File.extname(source_path)].join
+      target_path = File.join target_dirname, target_basename
+
+      _, chosen_path = BabDub::Prompts::Prompt.new(
+          question: "Proposed filename:\n  ".cyan,
+          lead: target_path,
+          regexp: //
+        ).ask
+
+      if chosen_path.empty?
+        puts ko.red + ' File skipped, bub.'
+      else
+        File.rename source_path, target_path unless BabDub::Options.get[:dry_run]
+        puts ok.green + ' ' + target_path
+      end
+    else
+      puts "No results, bub.".red
+      target_dirname = BabDub::Options.get[:reject_folder] || File.dirname(source_path)
+      target_basename = File.basename(source_path)
+      target_path = File.join target_dirname, target_basename
+
+      _, chosen_path = BabDub::Prompts::Prompt.new(
+          question: "Proposed filename:\n  ".cyan,
+          lead: target_path,
+          regexp: //
+        ).ask
+
+      if chosen_path.empty?
+        puts ko.red + ' File skipped, bub.'
+      else
+        File.rename source_path, target_path unless BabDub::Options.get[:dry_run]
+        puts ok.red + ' ' + target_path
+      end
+    end
+  end
 end
 
 puts "\n"
